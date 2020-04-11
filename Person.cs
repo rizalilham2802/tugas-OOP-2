f1ed97bf92da60717a66c20916ad7d199d31071a
mukhamad rizal <mukhamad.ilham@students.amikom.ac.id>
2020-04-11 09:06:53 +0700

    tugas oop2

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The Person has name : {0} and age : {1}", Name, Age);
+        }
+
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas
+{
+    class Teacher : Person
+    {
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+
+        public Teacher(string name, int age, string teacherid, string subject)
+            : base(name, age)
+        {
+            TeacherId = teacherid;
+            Subject = subject;
+        }
+    }
+}
